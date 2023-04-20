fafaaa69b8f50c1a7e845716dd4895179fb212e2
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2023-04-20 20:47:15 +0000

    Melhorias na calculadora e home

--- src/app/pages/calculadora/calculadora.page.ts
+++ src/app/pages/calculadora/calculadora.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HistoricoPage } from '../historico/historico.page';
 import { RouterModule } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-calculadora',
@@ -21,7 +22,7 @@ export class CalculadoraPage implements OnInit, AfterViewInit {
   binary2 = '';
   result = '';
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() { }
 
@@ -29,21 +30,30 @@ export class CalculadoraPage implements OnInit, AfterViewInit {
     console.log('historico:', this.historico);
   }
 
-  sum() {
-    const decimal1 = parseInt(this.binary1, 2);
-    const decimal2 = parseInt(this.binary2, 2);
-    const decimalSum = decimal1 + decimal2;
-    this.result = decimalSum.toString(2);
+  isBinary(val: string) {
+    return val.split('').filter(x => x == "0" || x == "1").length == val.length;
+  }
 
-    const operacao = `${this.binary1} + ${this.binary2} = ${this.result}`;
-    console.log(operacao);
+  async sum() {
 
-    this.historico.adicionarOperacao(operacao);
+    if (this.isBinary(this.binary1) && this.isBinary(this.binary2)) {
+
+      const decimal1 = parseInt(this.binary1, 2);
+      const decimal2 = parseInt(this.binary2, 2);
+      const decimalSum = decimal1 + decimal2;
+      this.result = decimalSum.toString(2);
+
+      const operacao = `${this.binary1} + ${this.binary2} = ${this.result}`;
+      console.log(operacao);
+
+      this.historico.adicionarOperacao(operacao);
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Erro',
+        message: 'Os números devem ser binários (0 ou 1)',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
 }
-
-
-
-
-
-
